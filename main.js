be28c8b41b1306a4428e9152afb10d5da3d7e9e9
Georgia <email.g@hey.com>
2022-07-03 15:17:05 +0100

    Added event listener for submitting a new grocery, this adds the groceryList as a JSON object in local storage

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 // Dynamically create shopping list from local storage
 window.addEventListener('load', () => {
-    groceryList = JSON.parse(localStorage.getItem('groceryList')) || [];
-})
+    let groceries = JSON.parse(localStorage.getItem('groceries')) || [];
 
+    document.querySelector('form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert('add button clicked!');
+        addGrocery(e.target.elements.newGrocery.value, groceries);
+    });
 
-const addGrocery = document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('add button clicked!');
-})
+    function addGrocery(newGrocery, groceryList) {
+        const grocery = {
+            nameOfGrocery: newGrocery,
+            bought: false
+        }
+        groceryList.push(grocery);
+        localStorage.setItem('groceries', JSON.stringify(groceryList));
+    }
 
-
-
+});
 
 // Validate input
 
