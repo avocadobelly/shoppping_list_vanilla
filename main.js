e6a58ab9bcea6cd504a1515f9d4dd5a1ec5c69f5
Georgia <email.g@hey.com>
2022-07-03 23:59:37 +0100

    Added fucntionality to update the shopping list and display it. Tidied up the styling a little with better spacing of elements.

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
 // Dynamically create shopping list from local storage
 window.addEventListener('load', () => {
-    let groceries = JSON.parse(localStorage.getItem('groceries')) || [];
+
+    groceries = JSON.parse(localStorage.getItem('groceries')) || [];
 
     document.querySelector('form').addEventListener('submit', (e) => {
+
         e.preventDefault();
-        alert('add button clicked!');
-        addGrocery(e.target.elements.newGrocery.value, groceries);
+        alert('its working!');
+        addGrocery(e.target.elements.new_grocery.value, groceries);
+        e.target.reset();
+        displayGroceryList(groceries);
+
     });
 
-    function addGrocery(newGrocery, groceryList) {
-        const grocery = {
-            nameOfGrocery: newGrocery,
-            bought: false
-        }
-        groceryList.push(grocery);
-        localStorage.setItem('groceries', JSON.stringify(groceryList));
-    }
+    displayGroceryList(groceries);
 
 });
 
+function displayGroceryList(groceriesArray) {
+    
+    const groceryListContainer = document.querySelector('#grocery_list_section');
+    groceryListContainer.innerHTML = "";
+
+    if (groceriesArray != []) {
+
+        groceryListContainer.innerHTML += `<ul class="wrapper flex_container" id="grocery_list"></ul>`;
+        groceriesArray.forEach(grocery => {  
+            document.querySelector('#grocery_list').innerHTML += `<li class="grocery">${grocery.nameOfGrocery}</li>`;
+        });
+
+    }
+}
+
+function addGrocery(grocery, list) {
+
+    const newGrocery = {
+        'nameOfGrocery': grocery,
+        'bought': false
+    }
+    list.push(newGrocery);
+    localStorage.setItem('groceries', JSON.stringify(list));
+
+}
+
 // Validate input
-
-
 // Sanitise input
